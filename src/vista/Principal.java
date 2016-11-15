1d2378174fc5ad5ff71ceeb9adc05a020b3a9cf5
Yojan Pardo <Yojan.Pardo@gmail.com>
2016-11-14 21:41:33 -0500

    arrglo tamaño del principal

--- src/vista/Principal.java
+++ src/vista/Principal.java
@@ -33,11 +33,11 @@ public class Principal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 772, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 394, Short.MAX_VALUE)
         );
 
         pack();
